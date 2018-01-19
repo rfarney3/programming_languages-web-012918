8d248e5586ec1c02285b2255fdab420f3eff8b18
Ryan Farney <ryanfarney@Ryans-MacBook-Pro-104.local>
2018-01-19 15:10:21 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,83 @@
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |func, language_hash|
+  	language_hash.each do |key, type|
+  		# binding.pry
+  		if new_hash[key] == nil
+  			new_hash[key] = type
+  		end
+
+  		if language_hash.include?(key) && new_hash[key][:style] == nil
+  			new_hash[key][:style] = [func]
+  		else 
+  			new_hash[key][:style] += [func]
+  		end
+  	end
+  end
+  new_hash
 end
+
+puts reformat_languages(languages)
